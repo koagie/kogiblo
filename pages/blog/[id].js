6d515dce4fdfb049d21bbf3d07fed8f30fc3ee9d
koagie <kogichan.pikapuuu.17.6@gmail.com>
2022-02-04 23:08:26 +0900

    add import styles

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,9 +1,9 @@
 import { client } from "../../libs/client";
-
+import styles from '../../styles/Home.module.scss';
 export default function BlogId({ blog }) {
     return (
-        <main className="{ styles.main}">
-            <h1 className="{ styles.title }">{blog.title}</h1>
+        <main className="{styles.main}">
+            <h1 className="{styles.title}">{blog.title}</h1>
             <p className="{styles.publishedAt}">{blog.publishedAt}</p>
             <div
                 dangerouslySetInnerHTML={{
@@ -27,7 +27,7 @@ export const getStaticPaths = async () => {
 export const getStaticProps = async (context) => {
     const id = context.params.id;
     const data = await client.get({ endpoint: "blog", contentId: id });
-``
+
     return {
         props: {
         blog: data,
